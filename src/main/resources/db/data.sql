df684ccf6979390cfd896daa4074922d54934fdd
Joonhee Jeong <jeonggoo75@gmail.com>
2023-10-18 00:24:38 +0900

    setup #3: 회원, 회사, 기술 더미데이터 추가

---
+++ src/main/resources/db/data.sql
@@ -0,0 +1,29 @@
+insert into company(name, country, city)
+values
+    ('원티드', '한국', '서울'),
+    ('쿠팡', '한국', '서울'),
+    ('삼성전자', '한국', '수원'),
+    ('Google', '미국', '마운틴 뷰'),
+    ('Apple', '미국', '쿠퍼티노'),
+    ('Vercel', '미국', '샌프란시스코')
+;
+
+insert into skill(name)
+values
+    ('JAVA'),
+    ('SPRING_FRAMEWORK'),
+    ('JPA'),
+    ('SQL'),
+    ('JAVASCRIPT'),
+    ('REACT'),
+    ('GIT'),
+    ('AWS'),
+    ('DOCKER')
+;
+
+insert into member(name, email)
+values
+    ('정준희', 'joonhee@example.com'),
+    ('홍길동', 'gildong@example.com'),
+    ('신사임당', 'saimdang@example.com')
+;

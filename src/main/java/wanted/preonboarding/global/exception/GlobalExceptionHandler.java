124ef44088138e9e6cede924b1d530f3f9c225f0
Joonhee Jeong <jeonggoo75@gmail.com>
2023-10-17 22:00:52 +0900

    feat #3: 글로벌 예외 핸들러 정의
    - 요청 DTO 바인딩 실패 시 예외처리

---
+++ src/main/java/wanted/preonboarding/global/exception/GlobalExceptionHandler.java
@@ -0,0 +1,27 @@
+package wanted.preonboarding.global.exception;
+
+import com.fasterxml.jackson.databind.exc.InvalidFormatException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import wanted.preonboarding.global.response.ApiMessageResponse;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    private static ResponseEntity<?> respondAsInvalidInput() {
+        return ApiMessageResponse.of(HttpStatus.BAD_REQUEST, "입력이 유효하지 않습니다.");
+    }
+
+    @ExceptionHandler
+    public ResponseEntity<?> handleJSONException(InvalidFormatException e) {
+        return respondAsInvalidInput();
+    }
+
+    @ExceptionHandler
+    public ResponseEntity<?> handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
+        return respondAsInvalidInput();
+    }
+}

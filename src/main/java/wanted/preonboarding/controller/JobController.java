bcce170197a6e533d73fe348db71b1755179eb49
Joonhee Jeong <jeonggoo75@gmail.com>
2023-10-18 14:13:52 +0900

    feat #4: 채용공고 수정 컨트롤러
    - 수정 요청 dto 정의

--- src/main/java/wanted/preonboarding/controller/JobController.java
+++ src/main/java/wanted/preonboarding/controller/JobController.java
@@ -4,11 +4,9 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import wanted.preonboarding.dto.JobRegisterDTO;
+import wanted.preonboarding.dto.JobUpdateDTO;
 import wanted.preonboarding.global.response.ApiMessageResponse;
 import wanted.preonboarding.service.JobService;
 
@@ -23,5 +21,14 @@ public class JobController {
     public ResponseEntity<?> register(@Valid @RequestBody JobRegisterDTO.Request dto) {
         jobService.register(dto);
         return ApiMessageResponse.of(HttpStatus.CREATED, "채용공고가 등록되었습니다.");
+    }
+
+    @PutMapping("/{jobId}")
+    public ResponseEntity<?> update(
+            @PathVariable Long jobId,
+            @Valid @RequestBody JobUpdateDTO.Request dto) {
+
+        jobService.update(jobId, dto);
+        return ApiMessageResponse.of(HttpStatus.OK, "채용공고가 수정되었습니다.");
     }
 }
---
+++ src/main/java/wanted/preonboarding/dto/JobUpdateDTO.java
@@ -0,0 +1,37 @@
+package wanted.preonboarding.dto;
+
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import wanted.preonboarding.domain.type.Career;
+import wanted.preonboarding.domain.type.JobGroup;
+import wanted.preonboarding.domain.type.JobPosition;
+import wanted.preonboarding.domain.type.SkillName;
+
+import java.util.List;
+
+public class JobUpdateDTO {
+
+    @Getter
+    public static class Request {
+
+        @NotNull
+        private JobGroup group;
+        @NotNull
+        private JobPosition position;
+        @NotNull
+        private Career career;
+        @NotNull
+        @Min(100_000)
+        private Integer reward;
+        @NotBlank
+        private String content;
+        @NotEmpty
+        private List<SkillName> skills;
+    }
+}
